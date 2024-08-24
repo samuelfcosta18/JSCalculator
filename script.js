b0c793ff9b813e5750ce2b9da52a09168d6888a2
Samuel Freitas <samuelfreitas7895@gmail.com>
2024-08-24 14:16:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function myFunction(event) {
-    if (event.key == '0' || event.key == '1' || event.key == '2' || event.key == '3'
-        || event.key == '4' || event.key == '5' || event.key == '6' || event.key == '7'
-        || event.key == '8' || event.key == '9' || event.key == '+' || event.key == '-'
-        || event.key == '/' || event.key == '*') {
+    if (event.key === '0' || event.key === '1' || event.key === '2' || event.key === '3'
+        || event.key === '4' || event.key === '5' || event.key === '6' || event.key === '7'
+        || event.key === '8' || event.key === '9' || event.key === '+' || event.key === '-'
+        || event.key === '/' || event.key === '*') {
         document.querySelector("textarea").value += event.key
     }
 }
